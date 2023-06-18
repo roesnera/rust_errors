1ce2cd29fadcde5d7d49e67d33f362480074b537
roesnera <adam.roesner@generation.org>
2023-06-18 12:12:18 -0400

    refactors using "?" operator

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,22 @@ use std::fs::File;
 use std::io::{self, Read};
 
 fn main() {
-    println!("Hello, world!");
+//    let username = match read_username_from_file() {
+//        Ok(string) => string,
+//        Err(e) => panic!("{}",e),
+//    };
+
+
+    let username = read_username_from_file_qm().unwrap_or_else(|err| {
+        panic!("{}",err)
+    });
+    println!("{username}");
 }
 
+// this is a crude way of error handling where we perform several operations that may result in an
+// error and handle those cases using match statements
 fn read_username_from_file() -> Result<String, io::Error> {
-    let username_file_result = File::open("ehhlo.txt");
+    let username_file_result = File::open("hello.txt");
 
     let mut username_file = match username_file_result {
         Ok(file) => file,
@@ -19,4 +30,12 @@ fn read_username_from_file() -> Result<String, io::Error> {
         Ok(_) => Ok(username),
         Err(e) => Err(e)
     }
+}
+
+// Rust provides a simpler way to do the exact same thing as above using the "?" operator
+fn read_username_from_file_qm () -> Result<String, io::Error> {
+    let mut username_file = File::open("hello.txt")?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
 }
